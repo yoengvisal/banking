494630ebb735900f16df300f602acd64b99a558f
yoengvisal <yoengvisal@gmail.com>
2025-11-16 11:03:45 +0700

    remove version package

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,13 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <encrypt_decrypt_plus/encrypt_decrypt_plus_plugin.h>
 #include <flutter_timezone/flutter_timezone_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
-  g_autoptr(FlPluginRegistrar) encrypt_decrypt_plus_registrar =
-      fl_plugin_registry_get_registrar_for_plugin(registry, "EncryptDecryptPlusPlugin");
-  encrypt_decrypt_plus_plugin_register_with_registrar(encrypt_decrypt_plus_registrar);
   g_autoptr(FlPluginRegistrar) flutter_timezone_registrar =
       fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterTimezonePlugin");
   flutter_timezone_plugin_register_with_registrar(flutter_timezone_registrar);
